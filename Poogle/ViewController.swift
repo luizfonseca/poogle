8d1b4d7e6b1f331c508ad486fdc19c18dfdae2b6
Luiz Fonseca <eu@luiz.cc>
2018-03-29 11:54:13 -0300

    Initial Commit

---
+++ Poogle/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Poogle
+//
+//  Created by Luiz Fonseca on 29/03/2018.
+//  Copyright © 2018 Luiz Fonseca. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
